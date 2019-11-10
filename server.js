84069e04554facbf79997646c209caf990ec536b
dxshim90 <dxshim90@gmail.com>
2019-11-10 18:29:12 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
